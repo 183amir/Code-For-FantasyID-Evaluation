a2d0b92e8f97256d0b842fbdd671f84fdfbee347
Amir Mohammadi <5738695+183amir@users.noreply.github.com>
2025-07-25 12:38:49 +0200

    Add validation set results and use threshold from val for FPR, FNR, and HTER

--- notebook.py
+++ notebook.py
@@ -6,44 +6,19 @@ app = marimo.App(width="full", app_title="Code For FantasyID Evaluation")
 
 @app.cell
 def _(
-    MODELS,
     format,
     hist_plot,
     metrics_df,
     mo,
-    px,
-    roc_df,
+    roc_plot,
+    selected_protocol,
+    selected_threshold,
     split,
     table_format,
 ):
-    def roc_plot(split):
-        df = roc_df(split)
-        df.model = df.model.replace(MODELS)
-        fig = px.line(
-            df,
-            x="fpr",
-            y="tpr",
-            color="model",
-            labels=dict(
-                fpr="False Positive Rate", tpr="True Positive Rate", model="Model"
-            ),
-            color_discrete_sequence=px.colors.qualitative.D3,
-        )
-        fig.update_layout(
-            legend=dict(
-                yanchor="bottom",
-                y=0.01,
-                xanchor="right",
-                x=0.99,
-                bgcolor="rgba(255,255,255,0.7)",
-                title=None,
-            )
-        )
-        return fig
-
     _plots = mo.vstack(
         [
-            mo.hstack([split, format]),
+            mo.hstack([split, selected_protocol, format]),
             mo.vstack(
                 [
                     mo.ui.plotly(
@@ -51,20 +26,20 @@ def _(
                         config={
                             "toImageButtonOptions": {
                                 "format": format.value,
-                                "filename": f"roc_{split.value}",
+                                "filename": f"roc_{split.value.replace(' ', '_')}",
                                 "height": 340,
                                 "width": 490,
                                 "scale": 2,
                             }
                         },
                     ),
-                    mo.hstack([split, format]),
+                    mo.hstack([split, selected_protocol, format]),
                     mo.ui.plotly(
                         hist_plot(split.value),
                         config={
                             "toImageButtonOptions": {
                                 "format": format.value,
-                                "filename": f"hist_{split.value}",
+                                "filename": f"hist_{split.value.replace(' ', '_')}",
                                 "height": 420,
                                 "width": 480,
                                 "scale": 2,
@@ -84,7 +59,9 @@ def _(
         "```\n" + metrics_df.to_latex(index=False, float_format="%.1f") + "\n```"
     )
     mo.md(f"""
+    # FantasyID Dataset Paper
     {_plots}
+    {mo.hstack([selected_protocol, selected_threshold, "The threshold for FPR, FNR, and HTER values are fixed from the validation set."])}
     {_table_md}
     {_table_latex}
     """)
@@ -92,10 +69,13 @@ def _(
 
 
 @app.cell
+def _(SPLITS, mo):
+    split = mo.ui.dropdown(options=SPLITS, value="all", label="Protocol")
+    return (split,)
+
+
+@app.cell
 def _(mo):
-    split = mo.ui.dropdown(
-        options=("all", "face", "text"), value="all", label="Protocol"
-    )
     format = mo.ui.dropdown(
         options=("png", "svg", "jpeg", "webp"),
         value="svg",
@@ -104,7 +84,13 @@ def _(mo):
     table_format = mo.ui.dropdown(
         options=("markdown", "latex"), value="markdown", label="Table Format"
     )
-    return format, split, table_format
+    selected_threshold = mo.ui.slider(
+        start=0, stop=1, step=0.01, value=0.5, label="Threshold"
+    )
+    selected_protocol = mo.ui.dropdown(
+        options=("val", "test"), value="test", label="Group"
+    )
+    return format, selected_protocol, selected_threshold, table_format
 
 
 @app.cell
@@ -131,52 +117,145 @@ def _():
 
 
 @app.cell
-def _(MODELS, pd):
+def _(MODELS, pd, selected_protocol):
+    def find_train_files(df):
+        val_set = df["protocol"] == "val"
+        last_split = df["filename"].str.split("-").str[-1]
+        return last_split.str.len() <= 11 & val_set
+
     def df_of_model(model):
         df = pd.read_parquet(
             f"https://raw.githubusercontent.com/183amir/Code-For-FantasyID-Evaluation/refs/heads/main/public/{model}/detection.parquet",
-            columns=("label", "score"),
-            filters=[("protocol", "==", "test")],
+            columns=("label", "score", "filename", "protocol"),
+            # filters=[("protocol", "==", selected_protocol.value)],
         )
+        train_mask = find_train_files(df)
+        df.loc[train_mask, "protocol"] = "train"
         df["model"] = model
         df["label"] = df["label"].replace(
             {
-                "facedancer": "face",
-                "digital_3": "text",
-                "textdiffuserft_bfei": "text",
+                "digital_3": "attack 1",
+                "facedancer": "attack 2",
+                "textdiffuserft_bfei": "attack 3",
+                "digital_1": "inswapper+diffste",
+                "digital_2": "facedancer+textdiffuser2",
             }
         )
         return df
 
     df_all_models = pd.concat([df_of_model(m) for m in MODELS])
+    SPLITS = ("all",) + tuple(
+        set(
+            df_all_models[
+                df_all_models["protocol"] == selected_protocol.value
+            ].label.unique()
+        )
+        - {"bonafide"}
+    )
+    print(SPLITS)
     print(df_all_models)
-    return (df_all_models,)
+    return SPLITS, df_all_models
 
 
 @app.cell
 def _(df_all_models):
     print(df_all_models.label.value_counts())
+    print(df_all_models.protocol.value_counts())
     return
 
 
 @app.cell
-def _(MODELS, df_all_models, pd, sklearn):
-    def get_df(model=None, split="all"):
-        if model is not None:
-            df = df_all_models[df_all_models.model == model]
-        else:
-            df = df_all_models.copy()
+def _(
+    MODELS,
+    SPLITS,
+    df_all_models,
+    np,
+    pd,
+    selected_protocol,
+    selected_threshold,
+    sklearn,
+):
+    def filter_df_by_split(df, split):
         labels = ["bonafide"]
         if split == "all":
-            labels.extend(["face", "text"])
+            labels.extend(set(SPLITS) - {"all"})
         else:
             labels.append(split)
         return df[df.label.isin(labels)]
 
-    def metrics(df):
+    def get_df(model=None, split="all", filter_by_protocol=True):
+        if model is not None:
+            df = df_all_models[df_all_models.model == model]
+        else:
+            df = df_all_models.copy()
+        if filter_by_protocol:
+            df = df[df["protocol"] == selected_protocol.value]
+        if split is None:
+            return df
+        return filter_df_by_split(df, split)
+
+    def far_threshold(negatives, positives, far_value=0.001, is_sorted=False):
+        """from bob.measure"""
+        if far_value < 0.0 or far_value > 1.0:
+            raise RuntimeError("`far_value' must be in the interval [0.,1.]")
+
+        if len(negatives) < 2:
+            raise RuntimeError("the number of negative scores must be at least 2")
+
+        epsilon = np.finfo(np.float64).eps
+        scores = negatives if is_sorted else np.sort(negatives)
+        if far_value >= (1 - epsilon):
+            return np.nextafter(scores[0], scores[0] - 1)
+
+        scores = np.flip(scores)
+        total_count = len(scores)
+        current_position = 0
+        valid_threshold = np.nextafter(
+            scores[current_position], scores[current_position] + 1
+        )
+        current_threshold = 0.0
+
+        while current_position < total_count:
+            current_threshold = scores[current_position]
+            while (
+                current_position < (total_count - 1)
+                and scores[current_position + 1] == current_threshold
+            ):
+                current_position += 1
+            future_far = (current_position + 1) / total_count
+            if future_far > far_value:
+                break
+            valid_threshold = current_threshold
+            current_position += 1
+
+        return valid_threshold
+
+    def compute_threshold(df):
         y_true = df["label"] == "bonafide"
         y_score = df["score"]
-        y_pred = y_score >= 0.5
+        negatives, positives = y_score[~y_true], y_score[y_true]
+        return far_threshold(negatives, positives, far_value=0.1)
+        # fpr, tpr, thresholds = sklearn.metrics.roc_curve(y_true, y_score)
+        # threshold_index = np.abs(fpr - 0.01).argmin()
+        # return thresholds[threshold_index]
+
+    def metrics(model, split):
+        df = get_df(model, split=None, filter_by_protocol=False)
+        val_threshold = compute_threshold(df[df["protocol"] == "val"])
+        df = df[df["protocol"] == selected_protocol.value]
+        df = filter_df_by_split(df, split)
+        y_true = df["label"] == "bonafide"
+        y_score = df["score"]
+        y_pred = y_score >= selected_threshold.value
+        tn, fp, fn, tp = (
+            sklearn.metrics.confusion_matrix(
+                y_true=y_true, y_pred=y_score >= val_threshold
+            )
+            .ravel()
+            .tolist()
+        )
+        fpr = fp / (fp + tn)
+        fnr = fn / (fn + tp)
         return {
             "ACC": sklearn.metrics.balanced_accuracy_score(
                 y_true=y_true, y_pred=y_pred
@@ -185,18 +264,23 @@ def _(MODELS, df_all_models, pd, sklearn):
             "F1": sklearn.metrics.f1_score(
                 y_true=y_true, y_pred=y_pred, average="weighted"
             ),
+            "FPR": fpr,
+            "FNR": fnr,
+            "HTER": (fpr + fnr) / 2,
         }
 
     def metrics_table():
         records = []
         for model in MODELS:
-            for split in ("all", "face", "text"):
-                df = get_df(model, split)
+            for split in SPLITS:
                 records.append(
                     {
                         "Model": model,
                         "Protocol": split,
-                        **{k: round(v * 100, 1) for k, v in metrics(df).items()},
+                        **{
+                            k: round(v * 100, 1)
+                            for k, v in metrics(model, split).items()
+                        },
                     }
                 )
         df = pd.DataFrame(records)
@@ -232,15 +316,40 @@ def _(MODELS, get_df, pd, sklearn, split):
 
 
 @app.cell
-def _(MODELS, get_df, go, np):
+def _(MODELS, px, roc_df):
+    def roc_plot(split):
+        df = roc_df(split)
+        df.model = df.model.replace(MODELS)
+        fig = px.line(
+            df,
+            x="fpr",
+            y="tpr",
+            color="model",
+            labels=dict(
+                fpr="False Positive Rate", tpr="True Positive Rate", model="Model"
+            ),
+            color_discrete_sequence=px.colors.qualitative.D3,
+        )
+        fig.update_layout(
+            legend=dict(
+                yanchor="bottom",
+                y=0.01,
+                xanchor="right",
+                x=0.99,
+                bgcolor="rgba(255,255,255,0.7)",
+                title=None,
+            )
+        )
+        return fig
+
+    return (roc_plot,)
+
+
+@app.cell
+def _(MODELS, SPLITS, get_df, go):
     def hist_plot(split):
         df = get_df(model=None, split=split)
-        df.label = df.label.replace(
-            {
-                "face": "attack",
-                "text": "attack",
-            }
-        )
+        df.label = df.label.replace({v: "attack" for v in SPLITS})
         df.model = df.model.replace(MODELS)
         unique_models = df["model"].unique()
 
@@ -250,7 +359,10 @@ def _(MODELS, get_df, go, np):
                 "fill": "rgba(31, 119, 180, 0.6)",
                 "line": "rgb(31, 119, 180)",
             },
-            "attack": {"fill": "rgba(255, 127, 14, 0.6)", "line": "rgb(255, 127, 14)"},
+            "attack": {
+                "fill": "rgba(255, 127, 14, 0.6)",
+                "line": "rgb(255, 127, 14)",
+            },
         }
 
         legend_map = {"bonafide": "Bonafide", "attack": "Attacks"}
@@ -339,6 +451,14 @@ def _(MODELS, get_df, go, np):
             ),
         )
 
+        # # add selected_threshold as a horizontal line here
+        # fig.add_hline(
+        #     y=selected_threshold.value,
+        #     line_color="red",
+        #     line_dash="dash",
+        #     annotation_text="Threshold",
+        #     annotation_position="top right",
+        # )
         return fig
 
     return (hist_plot,)
